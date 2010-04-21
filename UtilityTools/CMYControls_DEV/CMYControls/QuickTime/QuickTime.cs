176a8c70e38ce56accfa53f415c8ead7999c4a97
spoodygoon@gmail.com <andy@brdstudio.net>
2010-04-21 13:57:47 +0000

    Splitting off a dev and stable version

---
+++ UtilityTools/CMYControls_DEV/CMYControls/QuickTime/QuickTime.cs
@@ -0,0 +1,29 @@
+
+using System;
+
+namespace CMYControls
+{
+	
+	
+	[System.ComponentModel.ToolboxItem(true)]
+	public class QuickTime
+	{
+
+		public QuickTime()
+		{
+			
+			//this.Build();
+		}
+
+		
+		protected virtual void OnBtnQuickTimeClicked (object sender, System.EventArgs e)
+		{
+		}
+		
+		protected virtual void OnBtnClearTimeClicked (object sender, System.EventArgs e)
+				{
+			//txtTime.Text = "";
+				}		
+		
+	}
+}
