d8c8726ffc56d2ab921599d958bd3f4d80c9c1b8
spoodygoon <spoodygoon@6bbd4920-a155-0410-80b7-e32563a29f14>
2011-08-20 20:57:49 +0000

    * GlobalData/SQL.cs: * Schema/Database.cs:

--- SQLiteMonoPlus/SQLiteMonoPlusUI/Schema/Database.cs
+++ SQLiteMonoPlus/SQLiteMonoPlusUI/Schema/Database.cs
@@ -1,6 +1,9 @@
 using System;
+using System.IO;
+using System.Data;
 using System.Collections;
 using System.Collections.Generic;
+using Mono.Data.Sqlite;
 
 namespace SQLiteMonoPlusUI.Schema
 {
@@ -9,9 +12,31 @@ namespace SQLiteMonoPlusUI.Schema
 		private string _DatabaseFile = string.Empty;
 		public Dictionary<string, string> Pragmas = new Dictionary<string, string> ();
 		public TableCollection Tables = new TableCollection ();
+		public ForeignKeyCollection ForeignKeys = new ForeignKeyCollection();
+		public ViewCollection Views = new ViewCollection();
 		public Database (string DBFile)
 		{
 			_DatabaseFile = DBFile;
+			if(File.Exists(_DatabaseFile))
+			{
+				DataTable dt = LoadTables();
+				for(int i = 0; i < dt.Rows.Count; i ++)
+				{
+					// TODO load the rest of the data via Pragma
+				}
+			}
+		}
+		
+		private DataTable LoadTables()
+		{
+			SqliteConnection sqlCN = new SqliteConnection(_DatabaseFile);
+			SqliteCommand sqlCMD = new SqliteCommand(GlobalData.SQL.TablesGet, sqlCN);
+			sqlCMD.CommandType = CommandType.Text;
+			sqlCMD.CommandTimeout = 300;
+			SqliteDataAdapter sqlDA = new SqliteDataAdapter(sqlCMD);
+			DataTable dt = new DataTable();
+			sqlDA.Fill(dt);
+			return dt;
 		}
 	}
 
