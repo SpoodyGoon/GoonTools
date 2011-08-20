7311eb2bef1eb56e44a39fb6831fc1a4ab1f67bd
spoodygoon <andy@brdstudio.net>
2011-08-20 20:14:16 +0000

    * Schema/ForeignKey.cs: * Schema/ViewCollection.cs: * Schema/IndexCollection.cs: * Schema/ForeignKeyCollection.cs:

--- SQLiteMonoPlus/SQLiteMonoPlusUI/Schema/IndexCollection.cs
+++ SQLiteMonoPlus/SQLiteMonoPlusUI/Schema/IndexCollection.cs
@@ -1,11 +1,99 @@
+
 using System;
-namespace SQLiteMonoPlusUI
+using System.Collections;
+
+namespace SQLiteMonoPlusUI.Schema
 {
-	public class IndexCollection
+	public class IndexCollection : ICollection, IEnumerable
 	{
+		ArrayList _Indexs = new ArrayList();
 		public IndexCollection ()
 		{
 		}
+
+        public int Count
+        {
+            get { return _Indexs.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return _Indexs.IsReadOnly; }
+        }
+
+        public bool IsSynchronized
+        {
+            get { return _Indexs.IsSynchronized; }
+        }
+
+        public object SyncRoot
+        {
+            get { return this; }
+        }
+
+        public void Add(Index i)
+        {
+            _Indexs.Add(i);
+        }
+
+        public bool Contains(Index i)
+        {
+            return _Indexs.Contains(i);
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            _Indexs.CopyTo(array, index);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _Indexs.GetEnumerator();
+        }
+
+        public void Remove(Index i)
+        {
+            _Indexs.Remove(i);
+        }
+
+        public void Clear()
+        {
+            _Indexs.Clear();
+        }
+
+        public Index this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count)
+                    throw new ArgumentOutOfRangeException("index");
+
+                return (Index)_Indexs[index];
+            }
+        }
+
+        public Index this[string IndexName]
+        {
+            get
+            {
+				int index = -1;
+				for(int i = 0; i < _Indexs.Count; i++)
+				{
+					if(((Index)_Indexs[i]).IndexName == IndexName)
+					{
+						index = i;
+						break;
+					}
+				}
+				
+                if (index < 0 || index >= Count)
+                    throw new ArgumentOutOfRangeException("index");
+
+                return (Index)_Indexs[index];
+            }
+        }
 	}
 }
+
+
 
