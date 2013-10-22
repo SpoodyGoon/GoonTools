54b15eed0cb4f9802466aae91be5b7cb99784452
spoodygoon <andy@brdstudio.net>
2013-10-22 11:00:27 +0000

    Documentation updates, code cleanup, stylecop standardization.

--- SoftwareUpdate/GUPdotNET/Data/PackageInfo.cs
+++ SoftwareUpdate/GUPdotNET/Data/PackageInfo.cs
@@ -1,23 +1,26 @@
-//
-//  UpdateFileInfo.cs
-//
-//  Author:
-//       Andy York <goontools@brdstudio.net>
-//
-//  Copyright (c) 2013 Andy York
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// // --------------------------------------------------------------------------------------------------------------------
+// // <copyright file="PackageInfo.cs" company="Andy York">
+// //
+// // Copyright (c) 2013 Andy York
+// //
+// // This program is free software: you can redistribute it and/or modify
+// // it under the +terms of the GNU General Public License as published by
+// // the Free Software Foundation, either version 3 of the License, or
+// // (at your option) any later version.
+// //
+// // This program is distributed in the hope that it will be useful,
+// // but WITHOUT ANY WARRANTY; without even the implied warranty of
+// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// // GNU General Public License for more details.
+// //
+// // You should have received a copy of the GNU General Public License
+// // along with this program.  If not, see http://www.gnu.org/licenses/. 
+// // </copyright>
+// // <summary>
+// // Email: goontools@brdstudio.net
+// // Author: Andy York 
+// // </summary>
+// // --------------------------------------------------------------------------------------------------------------------
 
 namespace GUPdotNET.Data
 {
